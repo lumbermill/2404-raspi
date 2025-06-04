60afbd131cdb259365668680559852a8479f0470
ITO Yosei <y-itou@lumber-mill.co.jp>
2025-06-04 16:02:45 +0900

    Added script for carpool.

---
+++ yolo01-carpool/carpool.py
@@ -0,0 +1,44 @@
+import cv2, numpy, math, os, time
+
+storage = os.path.expanduser('~/carpool/storage')
+if not os.path.exists(storage):
+    os.makedirs(storage)
+cam = cv2.VideoCapture(0)
+
+from ultralytics import YOLO
+model = YOLO('yolo11n_ncnn_model')
+
+n_cars_prev, n_cars_now, streak = 0, 0, 0
+# 物体検出を行い、車の数をカウントする
+while True:
+    ret, image = cam.read()
+    results = model(image, verbose=False)
+    boxes = results[0].boxes
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    n_cars = 0
+    for b in boxes:
+        pt1 = (int(b.xyxy[0][0]),int(b.xyxy[0][1]))
+        pt2 = (int(b.xyxy[0][2]),int(b.xyxy[0][3]))
+        # 車は青の矩形で囲む、それ以外の物体はグレーの矩形で囲む
+        if b.cls == 2:
+            n_cars += 1
+            cv2.rectangle(image,pt1,pt2,(255,0,0),1)
+        else:
+            cv2.rectangle(image,pt1,pt2,(128,128,128),1)
+    m = "%d cars" % (n_cars)
+    cv2.putText(image,m,(10,460),font,1,(0,255,0),3,cv2.LINE_AA)
+
+    if n_cars == n_cars_now:
+        streak += 1
+    else:
+        streak = 0
+    n_cars_now = n_cars
+
+    if streak > 10 and n_cars_now != n_cars_prev:
+        f = "%s-%d.jpg" % (time.strftime('%y%m%d%H%M%S'), n_cars_now)
+        filename = os.path.join(storage, f)
+        cv2.imwrite(filename, image)
+        print("%d -> %d: Saved image to %s" % (n_cars_prev, n_cars_now, filename))
+        n_cars_prev = n_cars_now
+
+cam.release()
