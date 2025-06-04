f62dc7e64bc1b93bfab2940f24138ad71a08098e
ITO Yosei <y-itou@lumber-mill.co.jp>
2025-06-04 16:50:29 +0900

    wip: implementing front end for carpool.

---
+++ rails03-carpool/app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def root
+    @pictures = Dir.entries(Rails.root.join("public/pictures")).sort.reverse.select do |file|
+      file.match?(/\.(jpg|jpeg)$/i) && !file.start_with?('.')
+    end
+  end
+
+  def update
+    # This action is used to check if the application is running properly.
+    # It should return a 200 status code if everything is fine.
+    head :ok
+  end
+end
--- rails03-carpool/config/routes.rb
+++ rails03-carpool/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "pages/root"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,4 +12,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root "pages#root"
+  get "update" => "pages#update", as: :update
 end
