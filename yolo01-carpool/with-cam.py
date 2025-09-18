df31a18186ea6a1e4944396e671af1234431f302
ITO Yosei <y-itou@lumbermill.co.jp>
2025-09-18 15:10:57 +0900

    Improved the counter on carpool/withcam.py

--- yolo01-carpool/with-cam.py
+++ yolo01-carpool/with-cam.py
@@ -9,13 +9,19 @@ while True:
     ret, image = cam.read()
     results = model(image)
     boxes = results[0].boxes
-    m = "%d cars" % (len(boxes))
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(image,m,(10,460),font,1,(0,255,0),3,cv2.LINE_AA)
+    n_cars = 0
     for b in boxes:
         pt1 = (int(b.xyxy[0][0]),int(b.xyxy[0][1]))
         pt2 = (int(b.xyxy[0][2]),int(b.xyxy[0][3]))
-        cv2.rectangle(image,pt1,pt2,(255,0,0),1)
+        if b.cls == 2: # car
+            cv2.rectangle(image,pt1,pt2,(255,0,0),1)
+            n_cars += 1
+        else:
+            cv2.rectangle(image,pt1,pt2,(128,128,128),1)
+    m = "%d cars" % (n_cars)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(image,m,(10,460),font,1,(0,255,0),3,cv2.LINE_AA)
+
     cv2.imshow('yolo-carpool',image)
     k = cv2.waitKey(1)
     if k != -1:
