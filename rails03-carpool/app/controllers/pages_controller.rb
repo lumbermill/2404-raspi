bd67de4a3d2af40cc54a62d05f1fa5d98c78cbfb
ITO Yosei <y-itou@lumbermill.co.jp>
2025-09-18 16:37:06 +0900

    Added action cable feature.

--- rails03-carpool/app/controllers/pages_controller.rb
+++ rails03-carpool/app/controllers/pages_controller.rb
@@ -3,11 +3,15 @@ class PagesController < ApplicationController
     @pictures = Dir.entries(Rails.root.join("public/pictures")).sort.reverse.select do |file|
       file.match?(/\.(jpg|jpeg)$/i) && !file.start_with?('.')
     end
+    if @pictures.first
+      # Pick up the number of cars from file name. yyyymmddhhmmss-nn.jpg
+      @car_count = @pictures.first.match(/^\d+-(\d+).jpg$/)[1].to_i
+    end
   end
 
   def update
-    # This action is used to check if the application is running properly.
-    # It should return a 200 status code if everything is fine.
+    #Update carpool information
+    ActionCable.server.broadcast "notification_channel", {type: "carpool"}
     head :ok
   end
 end
